d95eed958f42e35a357ae1f2814d33b441f5ea8f
Andruten <andruten@users.noreply.github.com>
2023-03-28 16:36:51 +0200

    Bump python-telegram-bot version to 20.2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 import environ
 from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler, Updater
+from telegram.ext import Application, CallbackContext, CommandHandler
 
 from command_handlers import (add_service_command_handler, chat_services_checker_command_handler,
                               list_services_command_handler, remove_services_command_handler)
@@ -31,83 +31,81 @@ def error(update, context):
     logger.error('Update "%s" caused error "%s"', update, context.error)
 
 
-def check_all_services(context: CallbackContext):
+async def check_all_services(context: CallbackContext):
     chat_fetched_services = chat_services_checker_command_handler()
 
     for chat_id in chat_fetched_services:
         fetched_services = chat_fetched_services[chat_id]
         for unhealthy_service in fetched_services['unhealthy']:
             text = f'{unhealthy_service} is down 🤕!'
-            context.bot.send_message(chat_id=chat_id, text=text)
+            await context.bot.send_message(chat_id=chat_id, text=text)
         for healthy_service in fetched_services['healthy']:
             text = f'{healthy_service} is fixed now 😅!'
-            context.bot.send_message(chat_id=chat_id, text=text)
+            await context.bot.send_message(chat_id=chat_id, text=text)
 
 
-def add_service(update: Update, context: CallbackContext) -> None:
+async def add_service(update: Update, context: CallbackContext) -> None:
     if str(update.effective_chat.id) not in ALLOW_LIST_CHAT_IDS:
-        update.message.reply_text('You\'re not allowed to use this command')
+        await update.message.reply_text('You\'re not allowed to use this command')
         return
 
     # Validate arguments
     if len(context.args) != 4:
-        update.message.reply_text('Please, use /add <service_type> <name> <domain> <port>')
+        await update.message.reply_text('Please, use /add <service_type> <name> <domain> <port>')
         return
 
     service_type, name, domain, port = context.args
     if service_type.lower() not in HEALTHCHECK_BACKENDS.keys():
-        update.message.reply_text(f'<service_type> must be {", ".join(HEALTHCHECK_BACKENDS.keys())}')
+        await update.message.reply_text(f'<service_type> must be {", ".join(HEALTHCHECK_BACKENDS.keys())}')
         return
     try:
         port = int(port)
     except ValueError:
-        update.message.reply_text('<port> must be a number')
+        await update.message.reply_text('<port> must be a number')
         return
 
     service = add_service_command_handler(update.effective_chat.id, service_type, name, domain, port)
 
-    update.message.reply_text(f'ok! I\'ve added {service}')
+    await update.message.reply_text(f'ok! I\'ve added {service}')
 
 
-def remove_service(update: Update, context: CallbackContext) -> None:
+async def remove_service(update: Update, context: CallbackContext) -> None:
     if str(update.effective_chat.id) not in ALLOW_LIST_CHAT_IDS:
-        update.message.reply_text('You\'re not allowed to use this command')
+        await update.message.reply_text('You\'re not allowed to use this command')
         return
 
     if len(context.args) != 1:
-        update.message.reply_text('Please, use /remove <name>')
+        await update.message.reply_text('Please, use /remove <name>')
         return
     name, = context.args
 
-    remove_services_command_handler(name, update.effective_chat.id)
+    remove_services_command_handler(name, str(update.effective_chat.id))
 
-    update.message.reply_text(f'ok! I\'ve removed {name}')
+    await update.message.reply_text(f'ok! I\'ve removed {name}')
 
 
-def list_services(update: Update, context: CallbackContext) -> None:
+async def list_services(update: Update, context: CallbackContext) -> None:
     if str(update.effective_chat.id) not in ALLOW_LIST_CHAT_IDS:
-        update.message.reply_text('You\'re not allowed to use this command')
+        await update.message.reply_text('You\'re not allowed to use this command')
         return
 
-    services_str = list_services_command_handler(update.effective_chat.id)
+    services_str = list_services_command_handler(str(update.effective_chat.id))
 
-    update.message.reply_text(f'I\'m polling{"".join(services_str)}')
+    await update.message.reply_text(f'I\'m polling{"".join(services_str)}')
 
 
 def main() -> None:
-    updater = Updater(BOT_TOKEN)
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(BOT_TOKEN).build()
 
-    job = updater.job_queue
+    job = application.job_queue
     job.run_repeating(check_all_services, POLLING_INTERVAL)
 
-    dispatcher.add_handler(CommandHandler('add', add_service))
-    dispatcher.add_handler(CommandHandler('remove', remove_service))
-    dispatcher.add_handler(CommandHandler('list', list_services))
-    dispatcher.add_error_handler(error)
+    application.add_handler(CommandHandler('add', add_service))
+    application.add_handler(CommandHandler('remove', remove_service))
+    application.add_handler(CommandHandler('list', list_services))
+    application.add_error_handler(error)
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 
 if __name__ == '__main__':
