3c26b19bc023e489dbff119bd94f28468551426d
andruten <andruten@users.noreply.github.com>
2025-12-17 21:22:15 +0100

    fix: fetch response

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -16,37 +16,37 @@ class TestRequestBackend(unittest.TestCase):
     async def test_500(self):
         mock_session = MagicMock(status_code=500)
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertFalse(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertFalse(response.is_healthy)
 
     async def test_200(self):
         mock_session = MagicMock(status_code=200)
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertTrue(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertTrue(response.is_healthy)
 
     async def test_400(self):
         mock_session = MagicMock(status_code=400)
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertTrue(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertTrue(response.is_healthy)
 
     async def test_300(self):
         mock_session = MagicMock(status_code=300)
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertTrue(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertTrue(response.is_healthy)
 
     async def test_999(self):
         mock_session = MagicMock(status_code=999)
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertTrue(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertTrue(response.is_healthy)
 
     @patch('backends.logger')
     async def test_request_exception(self, mock_logger):
         mock_session = httpx.HTTPError
 
-        is_healthy, time_to_first_byte, expire_date = await self.backend.check(mock_session)
-        self.assertFalse(is_healthy)
+        response = await self.backend.check(mock_session)
+        self.assertFalse(response.is_healthy)
         mock_logger.warning.assert_called_once()
